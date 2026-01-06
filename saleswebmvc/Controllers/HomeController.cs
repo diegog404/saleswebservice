64e1aa598cbe0aae04fea975391528b11f662c0b
Diego Gomes <diegogns18@outlook.com>
2026-01-06 18:30:00 -0300

    added startup page, first controller tests

---
+++ saleswebmvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using saleswebmvc.Models;
+
+namespace saleswebmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Sobre"] = "Aluno Diego Gomes Nogueira da Silva";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ saleswebmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace saleswebmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
